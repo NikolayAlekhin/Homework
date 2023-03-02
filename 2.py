7936db9e3deb5efca0754afc29187766e6b399b3
Nikolay Alekhin <kolyan.alekhin@mail.ru>
2023-03-02 23:44:25 +0300

    'pers1'

---
+++ 2.py
@@ -0,0 +1,38 @@
+# В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+# В случае с английским алфавитом очки распределяются так:
+# A, E, I, O, U, L, N, S, T, R – 1 очко;
+# D, G – 2 очка;
+# B, C, M, P – 3 очка;
+# F, H, V, W, Y – 4 очка;
+# K – 5 очков;
+# J, X – 8 очков;
+# Q, Z – 10 очков.
+# А русские буквы оцениваются так:
+# А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+# Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка;
+# Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков;
+# Ш, Э, Ю – 8 очков;
+# Ф, Щ, Ъ – 10 очков
+
+
+
+word = input('Введите слово на русском или английском языке: ')
+
+if word[0].lower() in eng:
+    summa = 0
+    for letter in word:
+        for key, value in list_English.items():
+            if letter.upper() in value:
+                summa += key
+    print(f'стоимость введенного английского слова = {summa}')
+else:
+    if word[0].lower() in rus:
+        summa = 0
+        for letter in word:
+
+            for key, value in list_Russian.items():
+                if letter.upper() in value:
+                    summa += key
+print(f'стоимость введенного русского слова = {summa}')
